20bddb3c6129dca272f96b929e692893b29a48bf
Vanessa Pham <phamxvanessa@gmail.com>
2020-06-05 08:42:16 -0600

    Updates

---
+++ precache-manifest.c917c958a76aa550f1ae9f2ad21e8524.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46dc2286f60f4ec7eb1911a01abbb0c5",
+    "url": "/directory-app/index.html"
+  },
+  {
+    "revision": "8f873385dfb54595a7df",
+    "url": "/directory-app/static/css/main.2f23ba9b.chunk.css"
+  },
+  {
+    "revision": "48f071a2a8ee989d4e2e",
+    "url": "/directory-app/static/js/2.a807f56e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/directory-app/static/js/2.a807f56e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f873385dfb54595a7df",
+    "url": "/directory-app/static/js/main.10506058.chunk.js"
+  },
+  {
+    "revision": "5136db547c5072984e3b",
+    "url": "/directory-app/static/js/runtime-main.7bc6b035.js"
+  }
+]);
